38ffefc99ac22fd9d415ee6209cdebd0da267aba
Miguel Salazar <yobebe@hotmail.com.ar>
2023-10-18 20:19:35 -0300

    creando el repo del portafolio

---
+++ todo.js
@@ -0,0 +1,19 @@
+// Guardo el "ID" del botón
+const DescargaMiCv = document.getElementById("descarga-mi-cv");
+// Creo el evento "click" para cuando se descargue
+DescargaMiCv.addEventListener("click", descargarCv);
+// Creo la función para "descargarCv"
+function descargarCv() {
+    const urlCv = "./Mi cv/Miguel Salazar.pdf";
+    // Creo el enlace de descarga
+    const enlaceDescarga = document.createElement("a");
+    enlaceDescarga.href = urlCv;
+    enlaceDescarga.download = "Miguel Salazar.pdf";
+    enlaceDescarga.style.display = "none";
+    // Agrego el enlace al cuerpo del documento
+    document.body.appendChild(enlaceDescarga);
+    // Simulo un click en el botón de descarga
+    enlaceDescarga.click();
+    // Elimino el enlace del cuerpo del documento
+    document.body.removeChild(enlaceDescarga);
+}
